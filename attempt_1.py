9daf0ac57ddae34c5b32a8da045842380ee52102
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2023-04-14 17:51:51 +0300

    Add files via upload

--- attempt_1.py
+++ attempt_1.py
@@ -15,6 +15,19 @@ import os
 from utility.dataReader import data_reader
 from utility.dataReader import datasetToFolds
 
+def makePlot(history):
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.plot(history.history['MSE'])
+    plt.plot(history.history['val_MSE'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
 def main():
     class_names = ["sitting", "walking", "standing", "standingup", "sittingdown"]
     #class_namesDict = {"sitting" :1, "walking" :2, "standing":3, "standingup":4, "sittingdown":5}
@@ -45,26 +58,18 @@ def main():
         x = trainingData.iloc[:][["x1","y1","z1","x2","y2","z2","x3","y3","z3","x4","y4","z4"]]
         y = trainingData.iloc[:]["class"]
         test_data = np.array(data[i].iloc[:][["x1","y1","z1","x2","y2","z2","x3","y3","z3","x4","y4","z4"]])
-        test_labels = pd.get_dummies(data[i].iloc[:][["class"]])
-        history = model.fit(x, pd.get_dummies(y.iloc[:]), epochs=1, batch_size=1,
-                            validation_data = (test_data , test_labels) )
-        print(history.history.keys())
-        #predictions = model.predict(test_data)
-        #print(predictions)
+        test_labels = data[i].iloc[:][["class"]]
+        history = model.fit(x, pd.get_dummies(y.iloc[:]), epochs=5, batch_size=1,
+                            validation_data = (test_data , pd.get_dummies(test_labels.iloc[:] , columns=['class'])) )
+        makePlot(history)
         model.summary(show_trainable=True,)
 
     '''
     model= tf.keras.models.load_model('attempt_2_1')
     '''
     model.save('attempt_2_1')
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['MSE'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
+
+
 
 if __name__ == "__main__":
     main()
